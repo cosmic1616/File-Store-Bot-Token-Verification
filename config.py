b7a5c69a99f2213ab667a94c9059a004c251ef94
TestSub747 <aurigacosmic430@gmail.com>
2024-10-01 12:00:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "codeflix_bots")
 
 #Shortner (token system) 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "b3bcb9ce632a8cd2b5d944313f6a2b1ad0ae4c57")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "adrinolinks.in")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "4a548f4a5ad5f136e2d1c16044f4fcfc99d8b914")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/openshort_links/9") 
