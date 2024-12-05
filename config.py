3fee70836d84569ffedcd50ddb10f7a3a10204c8
cosmic1616 <cosmiccrazy16@gmail.com>
2024-12-05 20:12:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,11 +18,11 @@ DB_NAME = os.environ.get("DATABASE_NAME", "codeflix_bots")
 
 #Shortner (token system) 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.in")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "24d5076a1def399348764c4100e13b17eee127be")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "runurl.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "f20039ef51e42ddcf5f500aff0f7817490169e00")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/openshort_links/10") 
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/openshort_links/13") 
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002235598111"))
