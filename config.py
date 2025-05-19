b0cd89f071676e731f2ec9caa8bef820898c95d7
cosmic1616 <cosmiccrazy16@gmail.com>
2025-05-19 09:57:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/openshort_links/6") 
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002607681876"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-10026500775969"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -51,7 +51,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "Check Our New Site - Supplywalah.com"
+USER_REPLY_TEXT = "Check Our New Site - Supplywalah.infy.uk / desixxxvideos.infy.uk"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6952528517)
